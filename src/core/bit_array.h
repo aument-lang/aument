7f25c56bef0f5724bd9fb73b5904ed9b190e046f
fermian <78496870+fermian@users.noreply.github.com>
2021-02-13 09:56:55 +0700

    Fix buffer overflow from BA_LEN

--- src/core/bit_array.h
+++ src/core/bit_array.h
@@ -6,7 +6,7 @@
 #pragma once
 
 typedef char *bit_array;
-#define BA_LEN(n) ((n) / 8)
+#define BA_LEN(n) ((n+7) / 8)
 #define GET_BIT(x, n) ((((x)[(n) / 8]) & (0x1 << ((n) % 8))) != 0)
 #define SET_BIT(x, n) ((x)[(n) / 8]) |= (0x1 << ((n) % 8))
 #define RESET_BIT(x, n) ((x)[(n) / 8]) &= ~(0x1 << ((n) % 8))
