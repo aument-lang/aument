afafbad65607461f0c8c290568a79cdf777e3461
fermian <78496870+fermian@users.noreply.github.com>
2021-03-19 11:29:21 +0700

    `au_binary_path` function for finding Aument's path

--- src/platform/path.h
+++ src/platform/path.h
@@ -5,6 +5,10 @@
 // See LICENSE.txt for license information
 #pragma once
 #include "platform.h"
+#include "core/char_array.h"
 
 /// [func] Splits a path into file and directory components
 _Private int au_split_path(const char *path, char **file, char **wd);
+
+/// [func] Get the path that contains the running Aument executable
+_Private struct au_char_array au_binary_path();
