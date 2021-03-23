316b370e284655582513ce2ceab17ce9c5f9ee3f
chm8d <78496870+chm8d@users.noreply.github.com>
2021-03-23 16:38:05 +0700

    Fix merge conflict

--- src/core/rt/struct/helper.h
+++ src/core/rt/struct/helper.h
@@ -22,11 +22,7 @@ au_struct_idx_get(au_value_t value, au_value_t idx) {
     return retval;
 }
 
-<<<<<<< HEAD
-static _Unused _AlwaysInline void
-=======
 static AU_UNUSED AU_ALWAYS_INLINE void
->>>>>>> 2e45dbde0d66c3e458b86c83896ba8016999342c
 au_struct_idx_set(au_value_t value, au_value_t idx, au_value_t item) {
     struct au_struct *s = au_value_get_struct(value);
     if (AU_UNLIKELY(!s->vdata->idx_set_fn(s, idx, item))) {
