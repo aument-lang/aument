e2992cfb0e19a27666995d82af0a8c0c1c7baed6
fermian <78496870+fermian@users.noreply.github.com>
2021-03-24 08:01:42 +0700

    Add documentation for stdlib functions

--- src/stdlib/array.h
+++ src/stdlib/array.h
@@ -8,6 +8,24 @@
 
 #include "core/rt/extern_fn.h"
 
+/// [func-au] Repeats an element and stores that list of elements into an
+/// array
+/// @name array::repeat
+/// @param element the element to be repeated
+/// @param times the number of times the element is repeated
+/// @return The array
 AU_EXTERN_FUNC_DECL(au_std_array_repeat);
+
+/// [func-au] Pushes an element into an array
+/// @name array::push
+/// @param array the array
+/// @param element the element
+/// @return The array
 AU_EXTERN_FUNC_DECL(au_std_array_push);
+
+/// [func-au] Pops an element from an array and returns it
+/// @name array::pop
+/// @param array array
+/// @return The popped element from the array. If the array is empty, then
+/// it returns nil.
 AU_EXTERN_FUNC_DECL(au_std_array_pop);
