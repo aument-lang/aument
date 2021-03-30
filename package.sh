fdb75829d756426278c248cd6b450a141caf75b5
fermian <78496870+fermian@users.noreply.github.com>
2021-03-30 10:04:52 +0700

    Include license in distribution package

--- package.sh
+++ package.sh
@@ -13,17 +13,18 @@ MESONARG="-Dstatic_exe=true" CC=musl-gcc ./build.sh -f release
 cd build
 rm -f aument-linux64.zip
 cp "$main_dir/docs/package/README-linux.md" .
+cp "$main_dir/LICENSE.txt" .
 zip -9 aument-linux64.zip aument aument.h libau_runtime.a README-linux.md
 cd ..
 mv build/aument-linux64.zip dist/
+md5sum dist/aument-linux64.zip > dist/aument-linux64.md5
 
 ./build.sh -f release --cross-win64
 cd build
 rm -f aument-win64.zip
 cp "$main_dir/docs/package/README-windows.md" .
+cp "$main_dir/LICENSE.txt" .
 zip -9 aument-win64.zip aument.exe aument.h libaument.exe.a libau_runtime.a README-windows.md
 cd ..
 mv build/aument-win64.zip dist/
-
-md5sum dist/aument-linux64.zip > dist/aument-linux64.md5
 md5sum dist/aument-win64.zip > dist/aument-win64.md5
